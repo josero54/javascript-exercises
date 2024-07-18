be3630b50e1f25befe22a5692e51e79f98a78e31
josero54 <173284272+josero54@users.noreply.github.com>
2024-07-18 00:29:13 -0400

    Exercises 1, 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+     
+    if(repetitions >= 0){
+    let result = '';
+    for (let i = 1; i<= repetitions; i++){
+     result += string;
+    }
+    return result;
+} else {
+       return "ERROR";
+    }
 };
 
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    const text_splitted = string.split("");
+    reversed_text = text_splitted.reverse();
+    output_text = reversed_text.join('');
+    return output_text;
 
 };
 
+console.log(reverseString("Hello"));
 // Do not edit below this line
 module.exports = reverseString;
